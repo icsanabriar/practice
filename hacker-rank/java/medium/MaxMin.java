644195218f4486b76ee66fcdedcf0ed537f0a69b
icsanabriar <icsanabriar@googlemail.com>
2020-02-12 18:02:19 +0100

    REF - Reformat the code.

--- hacker-rank/java/medium/MaxMin.java
+++ hacker-rank/java/medium/MaxMin.java
@@ -62,17 +62,12 @@ public class MaxMin {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         int k = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int arrItem = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-            arr[i] = arrItem;
+            arr[i] = scanner.nextInt();
         }
 
         int result = solve(k, arr);
